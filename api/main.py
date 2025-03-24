1fe09cc89267e6769eeee89ae43bef4d22e661b0
zopHacks <zophacks@gmail.com>
2025-03-24 22:39:11 +0200

    Update main.py
    more allowed origins for render.com

--- api/main.py
+++ api/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://localhost", "http://localhost"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
